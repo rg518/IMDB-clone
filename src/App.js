e0c07c24917dd743077090b46d85cf9a49314d41
Rohit Gupta <rgcricket518@gmail.com>
2023-03-30 15:05:10 +0530

    HomePage added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
+import Home from "./Pages/home/home";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route index element={<h1>Hello!!</h1>}></Route>
+          <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<h1>Movie </h1>}></Route>
           <Route path="movies/:type" element={<h1>list</h1>}></Route>
           <Route path="/*" element={<h1>Error Page</h1>}></Route>
